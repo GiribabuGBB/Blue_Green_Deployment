aed37b794f4815b22ac8b13f71cabad1cfb7ff53
GiribabuGBB <139129775+GiribabuGBB@users.noreply.github.com>
2026-06-11 16:43:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-res.send('Version 5');
+res.send('Version 6 is updated');
 });
 
 app.get('/health',(req,res)=>{
-res.json({status:'UP'});
+res.json({status:'UP and Running'});
 });
 
 app.listen(3000);
